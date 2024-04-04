5f743406b6b033042d91632e0299034d723b0c13
MateusAndregtoni <131706994+MateusAndregtoni@users.noreply.github.com>
2024-04-04 17:13:03 -0300

    Aula 2 Rafael

--- Hello World/src/Main.java
+++ Hello World/src/Main.java
@@ -1,16 +1,51 @@
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        Scanner numero = new Scanner(System.in);
+//        System.out.println("Digite um numero:");
+//        int informacao = Integer.parseInt(numero.nextLine());
+//
+//        for (int i = 1; i <= 10; i++) {
+//            int result = i * informacao;
+//            System.out.printf("\n %d * %d = %d",i,informacao,result);
+//
+//        }
+//    }
+//}
+
+
+
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"OI");
+        int idade = Integer.parseInt(JOptionPane.showInputDialog("Receba!"));
+        JOptionPane.showConfirmDialog(null,"As vera?");
 
-        Scanner numero = new Scanner(System.in);
-        System.out.println("Digite um numero:");
-        int informacao = Integer.parseInt(numero.nextLine());
 
-        for (int i = 1; i <= 10; i++) {
-            int result = i * informacao;
-            System.out.printf("\n %d * %d = %d",i,informacao,result);
 
-        }
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Escreve:");
+        String info = teclado.nextLine();
+        System.out.println("Escreve Numero1:");
+        Float numero1 = teclado.nextFloat();
+        System.out.println("Escreve Numero2:");
+        Float numero2 = teclado.nextFloat();
+        System.out.println("Escreve Numero3:");
+        Float numero3 = teclado.nextFloat();
+        System.out.println("Escreve Numero4:");
+        Float numero4 = teclado.nextFloat();
+        float media = (numero1 + numero2 + numero3 + numero4) / 4;
+        if (media >= 7) {
+            System.out.printf(" %s sua media foi %.1f e voce passou!",info,media);
+
+        } else {
+            System.out.printf(" %s sua media foi %.1f e voce não passou!",info,media);
+        };
+
     }
 }

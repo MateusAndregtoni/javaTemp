7f756ae759952ba421f6164d1d471924b5379717
MATEUS ANDREGTONI DA SILVA <mateus_silva5@estudante.sc.senai.br>
2024-04-11 17:17:30 -0300

    arrays

--- Projeto01/src/Main.java
+++ Projeto01/src/Main.java
@@ -1,21 +1,64 @@
-import javax.swing.*;
+//import javax.swing.*;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+//     int numero,maior = -1;
+//
+//        for (int i = 0; i < 3; i++) {
+//            numero = Integer.parseInt(JOptionPane.showInputDialog((i+1) + "Numero: "));
+//            if (numero >= 0 && numero <= 100){
+//                if( numero > maior);
+//                maior = numero;
+//
+//            }else{
+//                i--;
+//            };
+//        }
+//
+//        JOptionPane.showMessageDialog(null,maior);
+//    }
+//}
+
+
+//import javax.swing.*;
+//import java.util.Arrays;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        int [] numeros = {1,2,3,4,5,6,7,8,9,10};
+//
+////        for (int i = 0; i < numeros.length; i++) {
+////            System.out.println("Array: " + numeros[i]);
+////        };
+//        int pesq = Integer.parseInt(JOptionPane.showInputDialog("Numero de array: "));
+//        int posicaoFake = Arrays.binarySearch(numeros,pesq);
+//
+//        JOptionPane.showMessageDialog(null , "olha: " + (posicaoFake + 1));
+//    };
+//};
+
+
+        import javax.swing.*;
+        import java.util.ArrayList;
+        import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        double maior;
 
-     int numero,maior = -1;
+        for (int i = 0; i < 5; i++) {
 
-        for (int i = 0; i < 3; i++) {
-            numero = Integer.parseInt(JOptionPane.showInputDialog((i+1) + "Numero: "));
-            if (numero >= 0 && numero <= 100){
-                if( numero > maior);
-                maior = numero;
-
-            }else{
-                i--;
-            };
+            ArrayList<Integer> salarios = new ArrayList<Integer>();
+            int salario = Integer.parseInt(JOptionPane.showInputDialog("Valor Salario:"));
+            salarios.add(salario);
         }
 
-        JOptionPane.showMessageDialog(null,maior);
-    }
-}
+
+
+    };
+};
+
+
+
